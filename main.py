1f121d2711a22328f7ecef5e0a64626211d7149c
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-07-13 23:17:15 -0700

    selection sort works

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ def bubblesort(array):
 test = [5,2,4,1,3]
 bubblesort(test)
 
+def selectionsort(array):
+    smallest = array[0]
+    for i in range(len(array)):
+        x = i+1
+        while x < len(array):
+            if array[i] < array[x]:
+                smallest = array[i]
+                x += 1
+            else:
+                smallest = array[x]
+                array[x], array[i] = array[i], array[x]
+    print(array)
+
+
+# Testing it
+test = [52,133,15,74]
+selectionsort(test)
+
 def insertionsort(array):
     for idx, number in enumerate(array):
         while idx > 0:
@@ -25,3 +43,6 @@ def insertionsort(array):
 # Testing it
 test = [5,2,3,1,4]
 insertionsort(test)
+
+
+
