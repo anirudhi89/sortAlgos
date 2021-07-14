0a0c38b91a51af0808761df97e61f399b557af36
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-07-13 22:53:03 -0700

    insertion sort works

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ def bubblesort(array):
 test = [5,2,4,1,3]
 bubblesort(test)
 
+def insertionsort(array):
+    for idx, number in enumerate(array):
+        while idx > 0:
+            if array[idx] < array[idx-1]:
+                array[idx-1], array[idx], = array[idx], array[idx-1]
+                idx -= 1
+                continue
+            break
+    print(array)
+# Testing it
+test = [5,2,3,1,4]
+insertionsort(test)
