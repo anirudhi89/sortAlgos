34dcab1063be15d812c2163ef02d06f09b4e50f1
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-07-13 22:39:59 -0700

    bubble sort works

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+def bubblesort(array):
+    n = len(array)
+    done = False
+    while not done:
+        done = True
+        for i in range(n-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                done = False
+    print(array)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Testing it
+test = [5,2,4,1,3]
+bubblesort(test)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
