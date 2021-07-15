4a1c43e6c63ce912a1bd78549ac06d7360e28ea1
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-07-14 20:32:25 -0700

    created slowsort

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def bubblesort(array):
     print(array)
 
 # Testing it
-test = [5,2,4,1,3]
-bubblesort(test)
+# test = [5,2,4,1,3]
+# bubblesort(test)
 
 def selectionsort(array):
     smallest = array[0]
@@ -26,10 +26,9 @@ def selectionsort(array):
                 array[x], array[i] = array[i], array[x]
     print(array)
 
-
 # Testing it
-test = [52,133,15,74]
-selectionsort(test)
+# test = [52,133,15,74]
+# selectionsort(test)
 
 def insertionsort(array):
     for idx, number in enumerate(array):
@@ -41,8 +40,41 @@ def insertionsort(array):
             break
     print(array)
 # Testing it
-test = [5,2,3,1,4]
-insertionsort(test)
+# test = [5,2,3,1,4]
+# insertionsort(test)
 
+def quicksort(array):
+#     need to do
+    x=5
 
+def slowsort(array):
+    #Create a slow sort algorithm that works, but with poor time complexity
+    from random import randint
+    #take two random numbers within the array, compare them, move the smaller to the start
+    flag = False
+    while not flag:
+        x = randint(0, len(array) - 1)
+        y = randint(0, len(array))
+        flag = True
+        if y < len(array):
+            if array[x] < array[y]:
+                array[0], array[x] = array[x], array[0]
+            else:
+                array[0], array[y] = array[y], array[0]
+        if array == sorted(array):
+            print("got here", array)
+            flag = True
+        else:
+            flag = False
+            print("not yet: ", array)
+    print(array)
 
+def testthese():
+    test = [5, 2, 3, 1, 4]
+    print("Order: Bubble, Selection, Insertion, Slow")
+    # bubblesort(test)
+    # selectionsort(test)
+    # insertionsort(test)
+    slowsort(test)
+
+testthese()
